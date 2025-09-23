c59d7a7b8afa9be228bca49ac86ce17c93675807
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-09-23 12:57:59 +0000

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+const { src, dest, series, watch } = require("gulp"); 
+const concat = require("gulp-concat");
+const svgSprite = require("gulp-svg-sprite");
+const imagemin = require("gulp-imagemin");
+const sourcemaps = require("gulp-sourcemaps");
+const { deleteAsync } = require("del");
+const browserSync = require("browser-sync").create();
+
+const clean = () => {
+    return deleteAsync(["build"]);
+}
+
+const html = () => {
+    return src("src/**/*.html")
+        .pipe(dest("build"))
+        .pipe(browserSync.stream())
+}
+
+const styles = () => {
+    return src("src/styles/**/*.css")
+        .pipe(sourcemaps.init())
+        .pipe(concat("main.css"))
+        .pipe(sourcemaps.write())
+        .pipe(dest("build"))
+        .pipe(browserSync.stream())
+}
+
+const svgSprites = () => {
+    return src("src/images/svg/**/*.svg")
+        .pipe(svgSprite({
+            mode: {
+                stack: {
+                    sprite: '../ivk-sprite.svg',
+                }
+            }
+        }))
+        .pipe(dest("build/images"))
+}
+
+const imagesMin = () => {
+    return src([
+        "src/images/**/*.jpg",
+        "src/images/**/*.jpeg",
+        "src/images/**/*.png",
+        "src/images/**/*.webp",
+        "src/images/*.svg",
+    ], { encoding: false })
+    .pipe(imagemin())
+    .pipe(dest("build/images"))
+}
+
+const watchFiles = () => {
+    browserSync.init({
+        server: {
+            baseDir: "build",
+        }
+    });
+}
+
+watch("src/**/*.html", html);
+watch("src/styles/**/*.css", styles);
+watch("src/images/svg/**/*.svg", svgSprites);
+
+exports.clean = clean;
+exports.html = html;
+exports.styles = styles;
+exports.default = series(clean, html, styles, imagesMin, svgSprites, watchFiles)
