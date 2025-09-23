8432d8feea1d7fcf155475f95f790f3eea4580d0
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-09-23 17:54:20 +0000

    starget page shop

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,73 @@
-const { src, dest, series, watch } = require("gulp"); 
+const { src, dest, series, watch } = require("gulp");
 const concat = require("gulp-concat");
 const svgSprite = require("gulp-svg-sprite");
 const imagemin = require("gulp-imagemin");
 const sourcemaps = require("gulp-sourcemaps");
-const { deleteAsync } = require("del");
 const browserSync = require("browser-sync").create();
 
-const clean = () => {
-    return deleteAsync(["build"]);
-}
+const clean = async () => {
+  const { deleteAsync } = await import("del");
+  return deleteAsync(["build"]);
+};
 
 const html = () => {
-    return src("src/**/*.html")
-        .pipe(dest("build"))
-        .pipe(browserSync.stream())
-}
+  return src("src/**/*.html").pipe(dest("build")).pipe(browserSync.stream());
+};
 
 const styles = () => {
-    return src("src/styles/**/*.css")
-        .pipe(sourcemaps.init())
-        .pipe(concat("main.css"))
-        .pipe(sourcemaps.write())
-        .pipe(dest("build"))
-        .pipe(browserSync.stream())
-}
+  return src("src/styles/**/*.css")
+    .pipe(sourcemaps.init())
+    .pipe(concat("main.css"))
+    .pipe(sourcemaps.write())
+    .pipe(dest("build"))
+    .pipe(browserSync.stream());
+};
 
 const svgSprites = () => {
-    return src("src/images/svg/**/*.svg")
-        .pipe(svgSprite({
-            mode: {
-                stack: {
-                    sprite: '../ivk-sprite.svg',
-                }
-            }
-        }))
-        .pipe(dest("build/images"))
-}
+  return src("src/images/svg/**/*.svg")
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: "../ivk-sprite.svg",
+          },
+        },
+      })
+    )
+    .pipe(dest("build/images"));
+};
 
 const imagesMin = () => {
-    return src([
-        "src/images/**/*.jpg",
-        "src/images/**/*.jpeg",
-        "src/images/**/*.png",
-        "src/images/**/*.webp",
-        "src/images/*.svg",
-    ], { encoding: false })
+  return src(
+    [
+      "src/images/**/*.jpg",
+      "src/images/**/*.jpeg",
+      "src/images/**/*.png",
+      "src/images/**/*.webp",
+      "src/images/*.svg",
+    ],
+    { encoding: false }
+  )
     .pipe(imagemin())
-    .pipe(dest("build/images"))
-}
+    .pipe(dest("build/images"));
+};
 
 const scripts = () => {
-    return src("src/js/main.js")
+  return src("src/js/main.js")
     .pipe(sourcemaps.init())
     .pipe(concat("app.js"))
     .pipe(sourcemaps.write())
     .pipe(dest("build"))
-    .pipe(browserSync.stream())
-}
+    .pipe(browserSync.stream());
+};
 
 const watchFiles = () => {
-    browserSync.init({
-        server: {
-            baseDir: "build",
-        }
-    });
-}
+  browserSync.init({
+    server: {
+      baseDir: "build",
+    },
+  });
+};
 
 watch("src/**/*.html", html);
 watch("src/styles/**/*.css", styles);
@@ -75,4 +78,12 @@ exports.clean = clean;
 exports.html = html;
 exports.styles = styles;
 exports.scripts = scripts;
-exports.default = series(clean, html, styles, scripts, imagesMin, svgSprites, watchFiles)
+exports.default = series(
+  clean,
+  html,
+  styles,
+  scripts,
+  imagesMin,
+  svgSprites,
+  watchFiles
+);
